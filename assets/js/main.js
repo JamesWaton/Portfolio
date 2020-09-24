03de8c29b82f158834693a33f507e5e3f7dc4d7e
JamesWaton <58121311+JamesWaton@users.noreply.github.com>
2020-09-24 11:04:13 +1000

    Portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+/*
+	Adding button to show video when cicking preview button
+*/
+function sudoku() {
+    var x = document.getElementById("sudokuVid");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function sudoku() {
+	var x = document.getElementById('sudokuVid');
+	if (x.style.visibility === 'hidden') {
+	  x.style.visibility = 'visible';
+	} else {
+	  x.style.visibility = 'hidden';
+	}
+  }
+
+
+
+function issue() {
+    var x = document.getElementById("issueVid");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function issue() {
+	var x = document.getElementById('issueVid');
+	if (x.style.visibility === 'hidden') {
+	  x.style.visibility = 'visible';
+	} else {
+	  x.style.visibility = 'hidden';
+	}
+  }
+
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function myFunction() {
+	var x = document.getElementById('myDIV');
+	if (x.style.visibility === 'hidden') {
+	  x.style.visibility = 'visible';
+	} else {
+	  x.style.visibility = 'hidden';
+	}
+  }
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('#nav a, .scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return $nav.height(); }
+		});
+
+})(jQuery);
